944eb20bfbbc51fae932c610bd73449010c20ec2
alhelal <al2helal@gmail.com>
2018-05-17 23:26:47 +0600

    add post OS installation script

---
+++ firefox_addon.sh
@@ -0,0 +1,3 @@
+wget https://addons.mozilla.org/firefox/downloads/file/909848/vimium_ff-1.63.3-an+fx.xpi
+firefox vimium_ff-1.63.3-an+fx.xpi
+rm vimium_ff-1.63.3-an+fx.xpi
---
+++ postInstallation.sh
@@ -0,0 +1,8 @@
+sudo apt-get install vim
+sudo apt-get install keepassx
+sudo apt-get install trash-cli
+sudo apt-get install git
+git config --global core.ignorecase false
+git config --global user.name "alhelal"
+git config --global user.email al2helal@gmail.com
+git config --global core.editor vim
