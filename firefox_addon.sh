cccc5845d43247c7e6249d0365cead3eecd10cb6
alhelal <al2helal@gmail.com>
2018-05-18 14:34:35 +0600

    add command to firefox_addon.sh and post.sh

--- firefox_addon.sh
+++ firefox_addon.sh
@@ -1,3 +1,12 @@
 wget https://addons.mozilla.org/firefox/downloads/file/909848/vimium_ff-1.63.3-an+fx.xpi
 firefox vimium_ff-1.63.3-an+fx.xpi
 rm vimium_ff-1.63.3-an+fx.xpi
+wget https://addons.mozilla.org/firefox/downloads/file/911315/uget_integration-2.1.3.1-an+fx-linux.xpi
+firefox uget_integration-2.1.3.1-an+fx-linux.xpi
+rm uget_integration-2.1.3.1-an+fx-linux.xpi
+wget https://addons.mozilla.org/firefox/downloads/file/957947/adblock_plus-3.1-an+fx.xpi
+firefox adblock_plus-3.1-an+fx.xpi
+rm adblock_plus-3.1-an+fx.xpi
+wget https://addons.mozilla.org/firefox/downloads/file/876142/duplicate_tabs_closer-3.1.5-fx.xpi
+firefox duplicate_tabs_closer-3.1.5-fx.xpi
+rm duplicate_tabs_closer-3.1.5-fx.xpi
--- postInstallation.sh
+++ postInstallation.sh
@@ -1,8 +1,13 @@
 sudo apt-get install vim
 sudo apt-get install keepassx
 sudo apt-get install trash-cli
-sudo apt-get install git
-git config --global core.ignorecase false
+sudo apt-get install git -y
+#git config --global core.ignorecase false
 git config --global user.name "alhelal"
 git config --global user.email al2helal@gmail.com
 git config --global core.editor vim
+sudo add-apt-repository ppa:ondrej/php
+sudo apt-get update
+sudo apt-get install php7.0 -y
+sudo apt-get install php7.0-mysql
+sudo phpenmod pdo_mysql
