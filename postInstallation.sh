5b9b3e6635842b877155bd9be1ca38519dc24cfb
alhelal <al2helal@gmail.com>
2019-12-04 20:02:05 +0600

    Update postInstallation.sh

--- postInstallation.sh
+++ postInstallation.sh
@@ -3,9 +3,9 @@ sudo apt-get install keepassx
 sudo apt-get install trash-cli
 sudo apt-get install git -y
 #git config --global core.ignorecase false
-git config --global user.name "alhelal"
-git config --global user.email al2helal@gmail.com
-git config --global core.editor vim
+git config --global user.name "alhelal" #set your github username
+git config --global user.email al2helal@gmail.com #set your github email
+git config --global core.editor vim #set your favourite editor
 sudo add-apt-repository ppa:ondrej/php
 sudo apt-get update
 sudo apt-get install php7.0 -y
