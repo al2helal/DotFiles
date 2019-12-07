4b99df6177303e286dd60d4e5a8f5e9874ecaaf7
alhelal <al2helal@gmail.com>
2019-12-08 00:51:01 +0600

    add savefrom, duplicate tab closer, grammerly etc.
    To firefox_addon.sh

--- firefox_addon.sh
+++ firefox_addon.sh
@@ -1,8 +1,21 @@
+#vimumm-ff
 wget https://addons.mozilla.org/firefox/downloads/file/909848/vimium_ff-1.63.3-an+fx.xpi
 firefox vimium_ff-1.63.3-an+fx.xpi
+#uget
 wget https://addons.mozilla.org/firefox/downloads/file/911315/uget_integration-2.1.3.1-an+fx-linux.xpi
 firefox uget_integration-2.1.3.1-an+fx-linux.xpi
+#adblock-plus
 wget https://addons.mozilla.org/firefox/downloads/file/957947/adblock_plus-3.1-an+fx.xpi
 firefox adblock_plus-3.1-an+fx.xpi
+#duplicate tab-closer
 wget https://addons.mozilla.org/firefox/downloads/file/876142/duplicate_tabs_closer-3.1.5-fx.xpi
 firefox duplicate_tabs_closer-3.1.5-fx.xpi
+#grammerly
+wget https://addons.mozilla.org/firefox/downloads/file/3421464/grammarly_for_firefox-8.857.0-an+fx.xpi
+firefox grammarly_for_firefox-8.857.0-an+fx.xpi
+#youtube-enhancer
+wget https://addons.mozilla.org/firefox/downloads/file/3395753/enhancer_for_youtubetm-2.0.98.2-fx.xpi
+firefox enhancer_for_youtubetm-2.0.98.2-fx.xpi
+#save from youtube downloader
+wget https://addons.mozilla.org/firefox/downloads/file/3455814/savefromnet-8.56-an+fx.xpi
+firefox savefromnet-8.56-an+fx.xpi
