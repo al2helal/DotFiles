ce3b6d32a120260edd9842b6f916a1918e2e565e
alhelal <al2helal@gmail.com>
2019-12-10 18:51:14 +0600

    update addblock plus  new version in firefox.sh

--- firefox_addon.sh
+++ firefox_addon.sh
@@ -5,8 +5,8 @@ firefox vimium_ff-1.63.3-an+fx.xpi
 wget https://addons.mozilla.org/firefox/downloads/file/911315/uget_integration-2.1.3.1-an+fx-linux.xpi
 firefox uget_integration-2.1.3.1-an+fx-linux.xpi
 #adblock-plus
-wget https://addons.mozilla.org/firefox/downloads/file/957947/adblock_plus-3.1-an+fx.xpi
-firefox adblock_plus-3.1-an+fx.xpi
+wget https://addons.mozilla.org/firefox/downloads/file/3429671/adblock_plus-3.7-an+fx.xpi
+firefox adblock_plus-3.7-an+fx.xpi
 #duplicate tab-closer
 wget https://addons.mozilla.org/firefox/downloads/file/876142/duplicate_tabs_closer-3.1.5-fx.xpi
 firefox duplicate_tabs_closer-3.1.5-fx.xpi
