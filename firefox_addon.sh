662a60fcafcc957b843f6f695aba23adf83453f3
alhelal <al2helal@gmail.com>
2020-02-14 14:47:50 +0600

    Update firefox_addon.sh

--- firefox_addon.sh
+++ firefox_addon.sh
@@ -1,6 +1,6 @@
 #vimumm-ff
-wget https://addons.mozilla.org/firefox/downloads/file/909848/vimium_ff-1.63.3-an+fx.xpi
-firefox vimium_ff-1.63.3-an+fx.xpi
+wget https://addons.mozilla.org/firefox/downloads/file/3502765/vimium_ff-1.65.1-fx.xpi
+firefox vimium_ff-1.65.1-fx.xpi
 #uget
 wget https://addons.mozilla.org/firefox/downloads/file/911315/uget_integration-2.1.3.1-an+fx-linux.xpi
 firefox uget_integration-2.1.3.1-an+fx-linux.xpi
